520de618593727aab61bea1e5a55d50d64a48bc4
yraucoules <yoann.raucoules@telecomnancy.eu>
2016-12-01 22:39:24 +0100

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name lanuitdelinfoFrontApp
+ * @description
+ * # lanuitdelinfoFrontApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('lanuitdelinfoFrontApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lanuitdelinfoFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the lanuitdelinfoFrontApp
+ */
+angular.module('lanuitdelinfoFrontApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
