c8a6f5de39995374d2288f30e4965fb870542de3
thomasgthy <thomas.gauthey@telecomnancy.net>
2016-12-02 01:32:30 +0100

    Ajout groupe

--- app/scripts/config/statesConfig.js
+++ app/scripts/config/statesConfig.js
@@ -3,27 +3,27 @@
 angular.module('lanuitdelinfoFrontApp')
     .config(['$stateProvider', 'States', '$urlRouterProvider',
     function ($stateProvider, States, $urlRouterProvider) {
-        
+
         // LOGIN
         var loginState = {
             url: '/login',
             templateUrl: 'views/partials/login.html',
             controller: 'LoginCtrl as loginCtrl'
         };
-        
+
         // ERROR 404 NOT FOUND
         var notFoundState = {
             url: '/notfound',
             templateUrl: 'views/partials/notfound.html'
         };
-        
+
         // Configure the route of the application
         var baseState = {
             abstract: true,
             url: '',
             templateUrl: 'views/main.html'
         };
-        
+
         // HOME
         var homeState = {
             url: '/home',
@@ -31,13 +31,21 @@ angular.module('lanuitdelinfoFrontApp')
             controller: 'HomeCtrl as homeCtrl',
             parent: baseState
         };
-        
+
+        var groupState = {
+            url: '/group',
+            templateUrl: 'views/partials/group.html',
+            controller: 'GroupCtrl as groupCtrl',
+            parent: baseState
+        };
+
         $stateProvider
             .state(States.BASE, baseState)
             .state(States.NOT_FOUND, notFoundState)
             .state(States.LOGIN, loginState)
-            .state(States.HOME, homeState);
-        
+            .state(States.HOME, homeState)
+            .state(States.GROUP, groupState);
+
         $urlRouterProvider.when('', homeState.url);
         $urlRouterProvider.when('/', homeState.url);
         $urlRouterProvider.otherwise(notFoundState.url);
--- app/scripts/constants/states.js
+++ app/scripts/constants/states.js
@@ -3,7 +3,8 @@
 angular.module('lanuitdelinfoFrontApp')
 .constant('States', {
     LOGIN: 'login',
-    NOT_FOUND: "notfound",
+    NOT_FOUND: 'notfound',
     BASE: 'dashboard',
-    HOME: 'dashboard.home'
+    HOME: 'dashboard.home',
+    GROUP: 'dashboard.group'
 });
